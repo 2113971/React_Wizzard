48c54aad3883393b217cbcb18dcc9d22ce260112
Ish-30 <ishika.rajcoomar@umail.uom.ac.mu>
2024-04-30 21:06:10 +0400

    Ishika - Sync

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Luqman from './Luqman'
+import Ishika from './Ishika'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,6 +24,7 @@ function App() {
           count is {count}
         </button>
         <p>
+          <Ishika/>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
